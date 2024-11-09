1bf58774e90648b291448a40006e95bf91c3bd8e
Ferhat <ferhattahar46@gmail.com>
2024-11-10 00:05:27 +0100

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div className=" font-poppins font-medium px-20 h-[100vh] w-full dark:bg-primary-800 bg-primary-900 dark:bg-grid-blue-200/[0.2] bg-grid-primary-500/[0.2] relative flexCenter">
+      <div className="absolute pointer-events-none inset-0 flexCenter dark:bg-primary-700 bg-primary-950 [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#030717]/60"></div>
+      <div className="relative z-20 flexCenter gap-2  flex-col  ">
+        <h1 className="  text-2xl xs:text-5xl sm:text-7xl  py-8 max-w-xl sm:max-w-3xl  text-center ">
+          Turning Ideas into{" "}
+          <span className="blue-gradient ">Digital Experiences</span>
+        </h1>
+        <p className="pt-2 pb-8 text-sm sm:text-base md:text-lg font-light text-stone-300/70 dark:text-stone-200/70 text-center">
+          Hi, I&apos;m ferhat, I create intuitive, visually stunning and highly
+          functional web applications.
+        </p>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,103 @@
-import type { Config } from "tailwindcss";
+import svgToDataUri from "mini-svg-data-uri";
+import { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
-export default {
+function addVariablesForColors({
+  addBase,
+  theme,
+}: {
+  addBase: any;
+  theme: any;
+}) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // Enables dark mode
   theme: {
     extend: {
+      screens: {
+        xxs: "300px",
+        xs: "380px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
+      borderRadius: {
+        "5xl": "40px",
+      },
       colors: {
+        "primary-100": "#B1C8E7", // Darker Light Blue
+        "primary-200": "#8BB8E4", // Darker Light Blue
+        "primary-300": "#6A9FEB", // Darker Soft Blue
+        "primary-400": "#4F86E2", // Darker Light Azure
+        "primary-500": "#2A6FDA", // Darker Primary Blue
+        "primary-600": "#1E50C6", // Darker Standard Blue
+        "primary-700": "#173A9B", // Darker Medium Blue
+        "primary-800": "#162C7D", // Darker Dark Blue
+        "primary-900": "#061059", //  Dark Blue
+        "primary-950": "#021038", // Very Dark Blue
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins: ["Poppins", "serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
+      matchUtilities(
+        {
+          "bg-grid": (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" cx="10" cy="10" r="1.6"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+};
+
+export default config;
