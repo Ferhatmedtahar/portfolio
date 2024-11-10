d5f117bf815786c54f56f94b9e974010700b5cc2
Ferhat <ferhattahar46@gmail.com>
2024-11-10 20:36:33 +0100

    gather images

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className=" font-poppins font-medium px-20 h-[100vh] w-full  bg-primary-900  bg-grid-primary-500/[0.2] relative flexCenter">
+    <div className=" font-poppins font-medium px-20 h-[99vh] w-full  bg-primary-900  bg-grid-primary-500/[0.2] relative flexCenter">
       <div className="absolute pointer-events-none inset-0 flexCenter  bg-primary-950 [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#030717]/60"></div>
       <div className="relative z-20 flexCenter gap-2  flex-col  ">
@@ -9,7 +9,7 @@ export default function Hero() {
           <span className="blue-gradient ">Digital Experiences</span>
         </h1>
         <p className="pt-2 pb-8 text-sm sm:text-base md:text-lg font-light text-stone-300/70  text-center">
-          Hi, I&apos;m ferhat, I create intuitive, visually stunning and highly
+          Hi, I&apos;m Ferhat, I create intuitive, visually stunning and highly
           functional web applications.
         </p>
       </div>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,9 +1,12 @@
+import { Project } from "../Project";
 export default function Projects() {
   return (
     <section className=" max-container padding-container pt-16 flex    flex-col gap-10">
       <h1 className="  tracking-[0.030rem]  text-2xl min-[430px]:text-2xl md:text-4xl font-medium  text-primary-300 underline  underline-offset-[6px] ">
         Projects
       </h1>
+
+      <Project />
     </section>
   );
 }
